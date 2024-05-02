a06e6eb5a22e23570886551ed2ad7189db8c670a
Somnath Chataraj <somnathchattaraj5@gmail.com>
2024-05-02 19:05:32 +0530

    removed node_modules

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const PubSubManager_1 = require("./PubSubManager");
-setInterval(() => {
-    PubSubManager_1.PubSubManager.getInstance().userSubscribe(Math.random().toString(), "APPL");
-}, 5000);

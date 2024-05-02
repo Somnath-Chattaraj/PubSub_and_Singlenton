48f0ede274a96f02caef7c5a09d93464993fcda1
Somnath Chataraj <somnathchattaraj5@gmail.com>
2024-05-02 18:56:27 +0530

    First Commit

---
+++ dist/logger.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.startLogger = void 0;
+const store_1 = require("./store");
+function startLogger() {
+    setInterval(() => {
+        setInterval(() => {
+            console.log(store_1.games);
+        }, 5000);
+    });
+}
+exports.startLogger = startLogger;
+;
